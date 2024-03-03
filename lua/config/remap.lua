4a48b663cf024bf1e6f77e9369a4691cf6488a88
Frenki <frenki@email.com>
2024-03-03 12:27:24 +0100

    Aditional remaps and inital settings. Implemented autocmd

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,9 +1,15 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- Navigate up and down in the middle
+vim.keymap.set("n", "<C-Down>", "<C-d>zz")
+vim.keymap.set("n", "<C-Up>", "<C-u>zz")
+-- Copy/Paste from + register
 vim.keymap.set({"n", "v"}, "<leader>p", "\"+p")
 vim.keymap.set({"n", "v"}, "<leader>P", "\"+P")
 vim.keymap.set({"n", "v"}, "<leader>y", "\"+y")
 vim.keymap.set({"n", "v"}, "<Del>", [["_<Del>]])
+-- move code lines
+vim.keymap.set("v", "<C-Down>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<C-Up>", ":m '<-2<CR>gv=gv")
+-- some helper functions
 vim.keymap.set({"n", "v"}, "<leader>ee", ":Ex<CR>")
-
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
