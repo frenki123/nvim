b8d0d11818fa08acd17ea80d0226849f0b3b7822
Frenki <frenki@email.com>
2024-03-17 16:05:56 +0100

    Added ruff in formaters. Installed linters

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -5,6 +5,7 @@ return {
         conform.setup {
             formatters_by_ft = {
                 lua = { "stylua" },
+                python = {"ruff_format"},
             },
         }
         vim.keymap.set(
