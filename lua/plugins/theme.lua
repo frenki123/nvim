50a2973beb4bf63a9981623539614233b82acdca
Frenki <frenki@email.com>
2024-04-07 18:04:57 +0200

    New theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,30 +1,30 @@
 return {
-    --    {
-    --        "catppuccin/nvim",
-    --        lazy = false,
-    --        priority = 1000,
-    --        config = function()
-    --            vim.cmd "colorscheme catppuccin-mocha"
-    --        end,
-    --    },
     {
-        "rose-pine/neovim",
+        "catppuccin/nvim",
         lazy = false,
         priority = 1000,
-        name = "rose-pine",
         config = function()
-            rose_pine = require "rose-pine"
-            rose_pine.setup {
-
-                styles = {
-                    bold = true,
-                    italic = true,
-                    transparency = true,
-                },
-            }
-            vim.cmd "colorscheme rose-pine-moon"
+            vim.cmd "colorscheme catppuccin-mocha"
         end,
     },
+    --    {
+    --        "rose-pine/neovim",
+    --        lazy = false,
+    --        priority = 1000,
+    --        name = "rose-pine",
+    --        config = function()
+    --            rose_pine = require "rose-pine"
+    --            rose_pine.setup {
+    --
+    --                styles = {
+    --                    bold = true,
+    --                    italic = true,
+    --                    transparency = true,
+    --                },
+    --            }
+    --            vim.cmd "colorscheme rose-pine-moon"
+    --        end,
+    --    },
     {
         "nvim-lualine/lualine.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
